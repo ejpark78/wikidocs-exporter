e451d65f3a76151cae5672809f70d016905fe4fc
eunjin.park <ejpark78@gmail.com>
2026-03-28 10:32:11 +0900

    change pack path

--- scripts/pack.js
+++ scripts/pack.js
@@ -6,8 +6,15 @@ console.log('Building extension...');
 require('child_process').execSync('npm run build', { stdio: 'inherit' });
 
 const distDir = path.join(__dirname, '..', 'dist');
-const outputFile = path.join(__dirname, '..', 'wikidocs-exporter.zip');
+const releaseDir = path.join(__dirname, '..', 'release'); // release 디렉토리 경로 정의
+const outputFile = path.join(releaseDir, 'wikidocs-exporter.zip');
 
+// 1. release 디렉토리가 없으면 생성 (recursive 옵션으로 상위 폴더까지 안전하게 생성)
+if (!fs.existsSync(releaseDir)) {
+  fs.mkdirSync(releaseDir, { recursive: true });
+}
+
+// 2. 기존 출력 파일이 있으면 삭제
 if (fs.existsSync(outputFile)) {
   fs.unlinkSync(outputFile);
 }
